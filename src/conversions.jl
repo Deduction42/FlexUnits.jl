c94ac7ed23bf80738dd09a6649a67d80eb8cf0b9
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-03-16 21:25:45 -0600

    Hint on generic conversion

--- src/conversions.jl
+++ src/conversions.jl
@@ -21,3 +21,16 @@ convert(::Type{<:Quantity}, q::UnionQuantity) = Quantity(q)
 convert(::Type{<:NumberQuantity}, q::UnionQuantity) = NumberQuantity(q)
 convert(::Type{<:RealQuantity}, q::UnionQuantity) = RealQuantity(q)
 
+
+
+#=
+uconvert(u::Units, q::UnionQuantity) will calculate the affine transformation, apply it and return the narrowest quantity in new units
+
+convert(::Quantity{Dimensions}, q::UnionQuantity) will uconvert(dimension(q), q) then apply Quantity
+convert(::Quantity{ScalarUnits}, q::UnionQuantity) will unconvert(ScalarUnits(scale=scale(q), dims=dimmension(q)), q), then apply Quantity
+convert(::Quantity{AffineUnits}, q::UnionQuantity) will ....
+convert(::Quantity{Dimensions}, q::AffineUnits) will fail if offset is not zero
+
+can potentially define:
+convert(::Type{Q}, q::UnionQuantity) where {U<:Dimensions,Q<:UnionQuantity{U}} = Q(uconvert(dimension(U), q)
+=#
