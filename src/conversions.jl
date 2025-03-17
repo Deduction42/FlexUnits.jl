7a4453ad4f7fa95459123768c3a004d543a884f1
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-03-16 21:06:22 -0600

    Cleaned up AffineTransformation

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,15 +1,13 @@
-@kwdef struct AffineTransformation
-    scale :: Float64
-    offset :: Float64
-end
-
-Base.muladd(x, affine::AffineTransformation) = muladd(x, affine.scale, affine.offset)
-
 """
     AffineTransformation(target::AbstractUnitLike, current::AbstractUnitLike)
 
 Calculates the affine transformation to convert a value from the current units to the target units
 """
+@kwdef struct AffineTransformation
+    scale :: Float64
+    offset :: Float64
+end
+
 function AffineTransformation(target::AbstractUnitLike, current::AbstractUnitLike)
     return AffineTransformation(
         scale  = uscale(current)/uscale(target),
@@ -17,6 +15,7 @@ function AffineTransformation(target::AbstractUnitLike, current::AbstractUnitLik
     )
 end
 
+Base.muladd(x, affine::AffineTransformation) = muladd(x, affine.scale, affine.offset)
 
 convert(::Type{<:Quantity}, q::UnionQuantity) = Quantity(q)
 convert(::Type{<:NumberQuantity}, q::UnionQuantity) = NumberQuantity(q)
