73f75b3620a948888f99f49baecf7a0c953d6fa4
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-12-18 18:43:55 -0700

    Fixed potential type-stability issue

--- src/math.jl
+++ src/math.jl
@@ -80,8 +80,8 @@ Base.:(==)(u1::AbstractAffineUnits, u2::AbstractAffineUnits) = (uscale(u1) == us
 @inline equaldims(arg1::AbstractDimensions) = arg1
 
 function equaldims(arg1::AbstractDimensions, arg2::AbstractDimensions)
-    (arg1, arg2) = promote(arg1, arg2)
-    return (arg1 == arg2) ? arg1 : throw(DimensionError((arg1,arg2)))
+    (d1, d2) = promote(arg1, arg2)
+    return (d1 == d2) ? d1 : throw(DimensionError((arg1,arg2)))
 end
 
 
