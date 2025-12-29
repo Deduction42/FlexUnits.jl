5048435ccd522a5440ce47a196dc33549e82443a
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-12-28 20:28:54 -0700

    Basic promotion works

--- src/static_units.jl
+++ src/static_units.jl
@@ -16,51 +16,52 @@ struct StaticDims{D} <: AbstractDimLike
 end 
 StaticDims(D::AbstractDimensions) = StaticDims{D}()
 StaticDims{D}(d::AbstractDimensions) where D = (D == d) ? StaticDims{D} : throw(ArgumentError("Dimesion $(d) must be equal to $(D)"))
-dimtype(::Type{StaticDims{D}}) where D= typeof(D)
+dimtype(::Type{StaticDims{D}}) where D = typeof(D)
+dimval(::Type{StaticDims{D}}) where D = D
+udynamic(u::StaticDims{D}) where D = D
 
-struct StaticUnit{D, C<:AbstractUnitConverter} <: AbstractUnitLike
+struct StaticUnits{D, C<:AbstractUnitConverter} <: AbstractUnitLike
     todims :: C
     symbol :: Symbol
-    function StaticUnit{D}(conv::C, symb=DEFAULT_USYMBOL::Symbol) where {D, C<:AbstractUnitConverter}
+    function StaticUnits{D}(conv::C, symb=DEFAULT_USYMBOL::Symbol) where {D, C<:AbstractUnitConverter}
         return (D isa AbstractDimensions) ? new{D,C}(conv, symb) : error("Type parameter must be a dimension")
     end
 end
-StaticUnit(u::AffineUnits) = StaticUnit{dimension(u)}(uconvert(dimension(u), u), u.symbol)
-dimtype(::Type{<:StaticUnit{D}}) where D = typeof(D)
+StaticUnits(u::AffineUnits) = StaticUnits{dimension(u)}(uconvert(dimension(u), u), u.symbol)
+AffineUnits(u::StaticUnits) = AffineUnits{dimtype(u)}(scale=u.todims.scale, offset=u.todims.offset, dims=dimval(u), symbol=u.symbol)
+udynamic(u::StaticUnits{D, U}) where {D, U<:AffineConverter} = AffineUnits(u)
+
+dimtype(::Type{StaticUnits{D,C}}) where {D,C} = typeof(D)
+dimval(::Type{StaticUnits{D,C}}) where {D,C} = D
 
 #============================================================================================================================
 Conversions
 ============================================================================================================================#
-uconvert(utarget::StaticDims{D}, ucurrent::StaticUnit{D}) where D = ucurrent.todims
+uconvert(utarget::StaticDims{D}, ucurrent::StaticUnits{D}) where D = ucurrent.todims
 
-function ubase(q::AbstractQuantity{T, StaticUnit{D}}) where {T,D}
+function ubase(q::AbstractQuantity{T, StaticUnits{D}}) where {T,D}
     x = unit(q).todims(ustrip(q))
     return Quantity{typeof(x), StaticDims{D}}(x, StaticDims{D}())
 end
 
-Quantity(x::T, u::StaticUnit{D}) where {T,D} = Quantity(u.todims(x), StaticDims{D}())
+Quantity(x::T, u::StaticUnits{D}) where {T,D} = Quantity(u.todims(x), StaticDims{D}())
 
 
-convert(::Type{D}, d::StaticDims{D0}) where {D0, D<:AbstractDimensions} = convert(D, D0)
-convert(::Type{Quantity{T,D}}, q::Quantity{<:Any, StaticDims{d}}) where {T,d,D<:AbstractDimensions} = Quantity{T,D}(ustrip(q), d)
+Base.convert(::Type{D}, d::StaticDims) where {D<:AbstractDimensions} = convert(D, dimval(d))
+Base.convert(::Type{Quantity{T,D}}, q::Quantity{<:Any, StaticDims{d}}) where {T,d,D<:AbstractDimensions} = Quantity{T,D}(ustrip(q), d)
 
-promote_rule(::D1, ::D2) where {D1<:AbstractDimensions, D2<:StaticDims} = D1
-promote_rule(::D1, ::D2) where {D1<:StaticDims, D2<:StaticDims} = promote_type(dimtype(D1), dimtype(D2))
+Base.promote_rule(::Type{D1}, ::Type{D2}) where {D1<:AbstractDimensions, D2<:StaticDims} = promote_type(D1, dimtype(D2))
+Base.promote_rule(::Type{D1}, ::Type{D2}) where {D1<:StaticDims, D2<:StaticDims} = promote_type(dimtype(D1), dimtype(D2))
 
 #============================================================================================================================
 Utilities
 ============================================================================================================================#
-function Base.show(io::IO, u::StaticDims{D}; pretty=PRETTY_DIM_OUTPUT[]) where D
-    return Base.show(io, D, pretty=pretty)
+function Base.show(io::IO, u::StaticDims; pretty=PRETTY_DIM_OUTPUT[])
+    return Base.show(io, udynamic(u), pretty=pretty)
 end
 
-function Base.show(io::IO, u::StaticUnit{D}; pretty=PRETTY_DIM_OUTPUT[]) where D
-    usymb = u.symbol
-    if usymb == DEFAULT_USYMBOL
-        return Base.show(io, D, pretty=pretty)
-    else
-        return Base.print(io, usymb)
-    end
+function Base.show(io::IO, u::StaticUnits; pretty=PRETTY_DIM_OUTPUT[])
+    return Base.show(ui, udynamic(u), pretty=pretty)
 end
 
 #============================================================================================================================
@@ -72,4 +73,4 @@ Promotions
 
 #Tests
 import .UnitRegistry.@u_str
-q = 1*StaticUnit(u"km/hr")
+q = 1*StaticUnits(u"km/hr")
