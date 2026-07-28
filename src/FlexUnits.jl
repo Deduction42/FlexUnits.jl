433143b125d287dce6d882838bf95b28bc33c14b
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2026-07-28 09:39:29 -0600

    Updated roadmap

--- src/FlexUnits.jl
+++ src/FlexUnits.jl
@@ -26,7 +26,7 @@ export static_fieldnames, uscale, uoffset, dimension, pretty_print_units
 export simplify, set_preferred_unit, set_preferred_logscale, display_simplified_units
 export with_ubase, ustrip, dstrip, ustrip_base, unit, quantity, logquant, linquant
 export ubase, uconvert, dconvert, scalar, udynamic, ustatic, logubase
-export QuantFieldArray, QuantFieldMatrix, QuantFieldVector, DimsMod, dimsmod
+export QuantFieldArray, QuantFieldMatrix, QuantFieldVector, DimsMod
 export LinmapQuant, VectorQuant, FactorQuant, FunctionQuant, UnitMap, DimsMap
 export uinput, uoutput, ufactor
 
